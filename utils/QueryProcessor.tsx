3921605459fcbe68aeedcaf282a89a9106891fd4
ANGRODHER <57401058+ANGRODHER@users.noreply.github.com>
2024-11-18 14:35:15 -0400

    Update QueryProcessor.tsx
    correcion 1

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,5 +1,7 @@
 export default function QueryProcessor(query: string): string {
-  if (query.toLowerCase().includes("shakespeare")) {
+  const lowerCaseQuery = query.toLowerCase();
+
+  if (lowerCaseQuery.includes("shakespeare")) {
     return (
       "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
       "English poet, playwright, and actor, widely regarded as the greatest " +
@@ -7,16 +9,18 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
-  if (query.toLowerCase().includes("usb id")) {
-    // TODO añade tu USB ID a continuación
-    // TODO actualiza el caso de prueba correspondiente en __tests__
-    return ( "15-11669" );
+  if (lowerCaseQuery.includes("usb id")) {
+    // TODO: añade tu USB ID a continuación
+    // TODO: actualiza el caso de prueba correspondiente en __tests__
+    return "15-11669";
   }
-  return "";
-  if (query.toLowerCase().includes("name")) {
-    // TODO añade tu USB ID a continuación
-    // TODO actualiza el caso de prueba correspondiente en __tests__
-    return ( "ANGRODHER2" );
+
+  if (lowerCaseQuery.includes("name")) {
+    // TODO: añade tu nombre a continuación
+    // TODO: actualiza el caso de prueba correspondiente en __tests__
+    return "ANGRODHER2";
   }
+
+  // Si no coincide con ningún caso
   return "";
 }
